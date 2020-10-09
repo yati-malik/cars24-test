7e72d2aee63f2a69b222dfb40417db3fc0bbff28
Yati Malik <yati.malik@pinelabs.com>
2020-10-09 14:43:11 +0530

    Updates

---
+++ precache-manifest.527edddb6a6f4d5febc1158b32b3ca90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f107d30e16a9ecd0414c35bc5e51ea",
+    "url": "/cars24-test/index.html"
+  },
+  {
+    "revision": "e83ac0958ed73b22b3d7",
+    "url": "/cars24-test/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "aa5fdafde16fe4fc0921",
+    "url": "/cars24-test/static/css/main.864b13fe.chunk.css"
+  },
+  {
+    "revision": "e83ac0958ed73b22b3d7",
+    "url": "/cars24-test/static/js/2.58d45838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cars24-test/static/js/2.58d45838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5fdafde16fe4fc0921",
+    "url": "/cars24-test/static/js/main.30859c30.chunk.js"
+  },
+  {
+    "revision": "dbbf15436dbd0f24189d",
+    "url": "/cars24-test/static/js/runtime-main.93c32d6a.js"
+  }
+]);
